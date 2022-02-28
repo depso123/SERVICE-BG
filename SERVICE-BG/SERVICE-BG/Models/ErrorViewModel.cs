081199fef47f904cf6c008ec5121d91732080956
depso123 <l.parvanov2017@pmg-pernik.com>
2022-02-28 14:47:53 +0200

    New

---
+++ SERVICE-BG/SERVICE-BG/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SERVICE_BG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
