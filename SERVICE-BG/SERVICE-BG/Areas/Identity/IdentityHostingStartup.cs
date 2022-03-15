dbaf1e82395e426ee93e882799923cb78cf01210
depso123 <l.parvanov2017@pmg-pernik.com>
2022-03-15 10:27:33 +0200

    new

---
+++ SERVICE-BG/SERVICE-BG/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SERVICE_BG.Data;
+
+[assembly: HostingStartup(typeof(SERVICE_BG.Areas.Identity.IdentityHostingStartup))]
+namespace SERVICE_BG.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
