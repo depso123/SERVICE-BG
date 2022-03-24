051f9d5c34a6fb4c3b9603f324a0775af56df2a3
depso123 <l.parvanov2017@pmg-pernik.com>
2022-03-24 12:56:44 +0200

    new

--- SERVICE-BG/SERVICE-BG/Controllers/HomeController.cs
+++ SERVICE-BG/SERVICE-BG/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace SERVICE_BG.Controllers
         {
             return View();
         }
+        public IActionResult Info()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ SERVICE-BG/SERVICE-BG/Entities/Service.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SERVICE_BG.Entities
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category  Category {get; set;}
+
+        public decimal Price { get; set; 
+        
+        }
+    }
+}
